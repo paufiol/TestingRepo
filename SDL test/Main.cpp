82dfc2f5d74bd8819524c35dc51bd9a9eebea393
paufiol <45202003+paufiol@users.noreply.github.com>
2019-02-27 12:18:47 +0100

    all backup

---
+++ SDL test/Main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "SDL/include/SDL.h"
+#pragma comment (lib, "SDL/Lib/SDL2main.lib")
+#pragma comment (lib, "SDL/Lib/SDL2.lib")
+
+
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* argv[])
+{
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL_Error: %s\n", SDL_GetError());
+	}
+	else {
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOW);
+		if (window == NULL) {
+			printf("SDL_Error: %s\n", SDL_GetError());
+		}
+		else {
+			screenSurface = SDL_GetWindowSurface(window);
+
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+			SDL_Delay(2000);
+		}
+	}
+	system("pause");
+	return 0;
+}
---
+++ SDL test/README.md
@@ -0,0 +1,13 @@
+# PDF-A  The Art Of Fighting
+Project I assignment
+
+# Art Of Fighting
+## Description
+The Art of fighting is a 1992 fighting game released by SNK. The game follows the trend of its time and is a 2D fighting game where one or two players can face NPCs or fight against each other. In order to win, the player has to knock out its enemy or have more health than it by the round's end. The game is always a best of three rounds. One of the main mechanics are combos and special attacks, which in this game are limited by a stamina bar. 
+
+## Gameplay Video:
+https://www.youtube.com/watch?v=QUnbLqb3Hac
+
+
+## Developers:
+Pau Fiol Lorente
