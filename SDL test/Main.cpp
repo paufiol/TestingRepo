e1ef37cdf955dfe171e3b0064162fc261ac32b1c
paufiol <45202003+paufiol@users.noreply.github.com>
2019-03-11 15:53:15 +0100

    Spaceship0.5

--- SDL test/Main.cpp
+++ SDL test/Main.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
-
+#include <stdio.h>
 #include "SDL/include/SDL.h"
 #include "SDL/include/SDL_image.h"
 
 
 #pragma comment (lib, "SDL/Lib/SDL2main.lib")
 #pragma comment (lib, "SDL/Lib/SDL2.lib")
-#pragma comment (lib, "SDL/lib/SDL2_image.lib")
+#pragma comment (lib, "SDL/Lib/SDL2_image.lib")
 
 
 using namespace std;
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 640;
 
 
 
-enum KeyPressSurfaces //creates constants starting from 0 in succession.
-{
-	KEY_PRESS_SURFACE_DEFAULT,
-	KEY_PRESS_SURFACE_UP,
-	KEY_PRESS_SURFACE_DOWN,
-	KEY_PRESS_SURFACE_LEFT,
-	KEY_PRESS_SURFACE_RIGHT,
-	KEY_PRESS_SURFACE_TOTAL
-};
+SDL_Texture* Loader(const char* file, SDL_Renderer* renderer) {
+	SDL_Surface* LoadSurf = NULL;
+	SDL_Texture* texture = NULL;
 
-bool KeyRepeat(SDL_Event e) {
-	bool ispressed = false;
-	if (e.type == SDL_KEYDOWN) {
-		ispressed = true; 
+	LoadSurf = IMG_Load(file); //"bg.png"
+	if (LoadSurf == NULL) {
+		cout << IMG_GetError() << endl;
+		return NULL;
 	}
-	return ispressed;
-}
 
+	texture = SDL_CreateTextureFromSurface(renderer, LoadSurf);
+	SDL_FreeSurface(LoadSurf);
+	return texture;
+}
 
 int main(int argc, char* argv[])
 {
@@ -41,10 +37,10 @@ int main(int argc, char* argv[])
 	
 	IMG_Init(IMG_INIT_PNG);
 
-	const int SCREEN_WIDTH = 640;
-	const int SCREEN_HEIGHT = 640;
-
 	const Uint8 *keystate = SDL_GetKeyboardState(NULL);
+
+	SDL_Renderer* renderer = NULL;
+	
 
 	SDL_Window* window = NULL;
 	window = SDL_CreateWindow
@@ -57,23 +53,35 @@ int main(int argc, char* argv[])
 		SDL_WINDOW_SHOWN
 	);
 
-	SDL_Renderer* renderer = NULL;
+
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-	SDL_RenderClear(renderer);
+
+	SDL_Surface* LoadSurf = NULL;
+	SDL_Texture* bg = NULL;
+	SDL_Texture* ship = NULL;
+	SDL_Texture* projectile = NULL;
+	
+	bg = Loader("bg.png", renderer);
+	ship = Loader("nave.png", renderer);
+	projectile = Loader("projectile.png", renderer);
 
 	SDL_Rect r;
 	r.x = 270;
 	r.y = 270;
-	r.w = 100;
-	r.h = 100;
+	r.w = 200;
+	r.h = 200;
 
 	SDL_Rect bullet[30];
 	for (int i = 0; i < 30; i++) {
-		bullet[i] = { NULL, -20, 50, 15 };
+		bullet[i] = { NULL, -200, 200, 200};
 	}
 	
-	
+
+
+
+
+
 	bool up = false, down = false, right = false, left = false;
 
 	int i = 0; 
@@ -90,34 +98,33 @@ int main(int argc, char* argv[])
 			if (e.type == SDL_QUIT) {
 				quit = true;
 			}
-			if(e.key.keysym.sym == SDLK_SPACE){
-				if (e.type == SDL_KEYDOWN && timer > 20) {
-				fire = true;
-				bullet[i].x = r.x + 45;
-				bullet[i].y = r.y + 45;							
-				i++;
-				i = i % 30;
-				timer = 0;
-				}
-			break;
-			}
-		
 		}
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderClear(renderer);
+		
+		SDL_RenderCopy(renderer, bg, NULL, NULL);
 
 		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 		
 		if (fire) {
 			for (int j = 0; j < 30; j++) {
-				if (bullet[j].x < 640) {
-					SDL_RenderFillRect(renderer, &bullet[j]);
+				if (bullet[j].x < SCREEN_WIDTH) {
+					//SDL_RenderFillRect(renderer, &bullet[j]);
+					SDL_RenderCopy(renderer, projectile, NULL, &bullet[j]);
 					bullet[j].x += 5;
 				}
 			}
 		}
 
-		//if (keystate[SDL_SCANCODE_SPACE]) { bullet -= 3; }
+		if (keystate[SDL_SCANCODE_SPACE] && timer > 45) { 
+			
+			fire = true;
+			bullet[i].x = r.x + 100;
+			bullet[i].y = r.y ;
+			i++;
+			i = i % 30;
+			timer = 0;
+		}
+		
 		if (keystate[SDL_SCANCODE_UP]) { r.y -= 3; }
 		if (keystate[SDL_SCANCODE_DOWN]) { r.y += 3; }
 		if (keystate[SDL_SCANCODE_LEFT]) { r.x -= 3; }
@@ -125,18 +132,17 @@ int main(int argc, char* argv[])
 
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-		SDL_RenderFillRect(renderer, &r);
+		SDL_RenderCopy(renderer, ship, NULL, &r);
 		SDL_RenderPresent(renderer);
 	}
+	
 	window = NULL;
 	renderer = NULL;
 	SDL_DestroyWindow(window);
-
+	SDL_DestroyTexture(bg);
 
 	SDL_Quit();
 	IMG_Quit();
-
-
 
 	return 0;
 }
